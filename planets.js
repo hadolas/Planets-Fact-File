a5233575168cd80ded34347ee1db0ead31b69002
Hadolas <hadiyahmughal@gmail.com>
2019-06-14 16:42:34 +0100

    Refactor code

--- planets.js
+++ planets.js
@@ -18,6 +18,15 @@ function Planet(name, img, radius, orbital_period, moons, day_length) {
 	this.day_length = day_length;
 }
 
+function updatePlanetDetails(planets_array_pointer) {
+	planet_name.textContent = planets_array[planets_array_pointer].name;
+	planet_img.src = planets_array[planets_array_pointer].img;
+	planet_radius.textContent = planets_array[planets_array_pointer].radius;
+	planet_orbital_period.textContent = planets_array[planets_array_pointer].orbital_period;
+	planet_moons.textContent = planets_array[planets_array_pointer].moons;
+	planet_day_length.textContent = planets_array[planets_array_pointer].day_length;
+}
+
 var planets_array = [];
 
 var Mercury = new Planet("Mercury", "../imgs/mercury.png", "2,439km", "88 Earth days", "None", "58d 15h 30m");
@@ -28,54 +37,32 @@ var Earth = new Planet("Earth", "../imgs/mercury.png", "0km", "0 Earth days", "0
 planets_array.push(Earth);
 
 var planets_array_length = planets_array.length;
-
-// console.log(planets_array);
-// console.log(planets_array_length);
-
-planet_name.textContent = Mercury.name;
-planet_img.src = Mercury.img;
-planet_radius.textContent = Mercury.radius;
-planet_orbital_period.textContent = Mercury.orbital_period;
-planet_moons.textContent = Mercury.moons;
-planet_day_length.textContent = Mercury.day_length;
-
 var planets_array_pointer = 0;
+
+updatePlanetDetails(planets_array_pointer);
 
 // button_left
 button_left.addEventListener("click", function() {
-	// console.log(planets_array_pointer);
+
 	if(planets_array_pointer===0){
 		planets_array_pointer=planets_array_length-1;
 	} else {
 		planets_array_pointer--;
 	}
-	
-	// console.log(planets_array_pointer);
 
-	planet_name.textContent = planets_array[planets_array_pointer].name;
-	planet_img.src = planets_array[planets_array_pointer].img;
-	planet_radius.textContent = planets_array[planets_array_pointer].radius;
-	planet_orbital_period.textContent = planets_array[planets_array_pointer].orbital_period;
-	planet_moons.textContent = planets_array[planets_array_pointer].moons;
-	planet_day_length.textContent = planets_array[planets_array_pointer].day_length;
+	updatePlanetDetails(planets_array_pointer);
 });
 
 // button_right
 button_right.addEventListener("click", function() {
-	// console.log(planets_array_length-1);
+
 	if(planets_array_pointer===planets_array_length-1) {
 		planets_array_pointer=0;
 	} else {
-		console.log(planets_array_pointer);
 		planets_array_pointer++;
 	}
 
-	planet_name.textContent = planets_array[planets_array_pointer].name;
-	planet_img.src = planets_array[planets_array_pointer].img;
-	planet_radius.textContent = planets_array[planets_array_pointer].radius;
-	planet_orbital_period.textContent = planets_array[planets_array_pointer].orbital_period;
-	planet_moons.textContent = planets_array[planets_array_pointer].moons;
-	planet_day_length.textContent = planets_array[planets_array_pointer].day_length;
+	updatePlanetDetails(planets_array_pointer);
 });
 
 
